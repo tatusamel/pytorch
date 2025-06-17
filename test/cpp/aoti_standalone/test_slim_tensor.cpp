b6356da0065cd4ecfd6693974c97b9fe0d86f565
Serhat Gundem <tatusamell@gmail.com>
2025-06-17 14:11:53 -0700

    lint test_slim_tensor.cpp

--- test/cpp/aoti_standalone/test_slim_tensor.cpp
+++ test/cpp/aoti_standalone/test_slim_tensor.cpp
@@ -10,42 +10,43 @@ namespace torch::standalone {
 using ::testing::ElementsAreArray;
 
 TEST(SlimTensorInternalTest, SetSizesContiguous) {
-    SlimTensor tensor = create_empty_tensor({}, {}, c10::kFloat);
-    EXPECT_EQ(tensor.numel(), 1);
+  SlimTensor tensor = create_empty_tensor({}, {}, c10::kFloat);
+  EXPECT_EQ(tensor.numel(), 1);
 
   std::vector<int64_t> new_size_vec = {2, 3, 4};
   c10::IntArrayRef new_sizes(new_size_vec.data(), new_size_vec.size());
   tensor.set_sizes_contiguous(new_sizes);
 
-    EXPECT_EQ(tensor.dim(), 3);
-    EXPECT_EQ(tensor.numel(), 24);
+  EXPECT_EQ(tensor.dim(), 3);
+  EXPECT_EQ(tensor.numel(), 24);
 
-    EXPECT_THAT(tensor.sizes(), ElementsAreArray({2, 3, 4}));
-    EXPECT_THAT(tensor.strides(), ElementsAreArray({12, 4, 1}));
-    EXPECT_TRUE(tensor.is_contiguous());
-
+  EXPECT_THAT(tensor.sizes(), ElementsAreArray({2, 3, 4}));
+  EXPECT_THAT(tensor.strides(), ElementsAreArray({12, 4, 1}));
+  EXPECT_TRUE(tensor.is_contiguous());
 }
 
-
 TEST(SlimTensorInternalTest, SetSizesAndStridesNonContiguous) {
+  SlimTensor tensor = create_empty_tensor({}, {}, c10::kFloat);
 
-    SlimTensor tensor = create_empty_tensor({}, {}, c10::kFloat);
+  // Set sizes and strides to represent a transposed tensor.
+  // This is equivalent to a (2, 4) tensor that has been transposed to (4, 2).
+  std::vector<int64_t> new_size_vec = {4, 2};
 
   // Strides of original (2, 4) were {4, 1}
   std::vector<int64_t> new_stride_vec = {1, 4};
   c10::IntArrayRef new_sizes(new_size_vec.data(), new_size_vec.size());
   c10::IntArrayRef new_strides(new_stride_vec.data(), new_stride_vec.size());
 
-    tensor.set_sizes_and_strides(new_sizes, new_strides);
+  tensor.set_sizes_and_strides(new_sizes, new_strides);
 
-    // verify
-    EXPECT_EQ(tensor.dim(), 2);
-    EXPECT_EQ(tensor.numel(), 8);
-    EXPECT_THAT(tensor.sizes(), ElementsAreArray({4, 2}));
-    EXPECT_THAT(tensor.strides(), ElementsAreArray({1, 4}));
+  // verify
+  EXPECT_EQ(tensor.dim(), 2);
+  EXPECT_EQ(tensor.numel(), 8);
+  EXPECT_THAT(tensor.sizes(), ElementsAreArray({4, 2}));
+  EXPECT_THAT(tensor.strides(), ElementsAreArray({1, 4}));
 
-    // A transposed tensor is not contiguous.
-    EXPECT_FALSE(tensor.is_contiguous());
+  // A transposed tensor is not contiguous.
+  EXPECT_FALSE(tensor.is_contiguous());
 }
 
 TEST(SlimTensorInternalTest, EmptyTensorRestride) {
@@ -58,19 +59,18 @@ TEST(SlimTensorInternalTest, EmptyTensorRestride) {
   // it shouldn't be contiguous first
   EXPECT_FALSE(tensor.is_contiguous());
 
-    // make the tensor contiguous.
-    tensor.empty_tensor_restride(c10::MemoryFormat::Contiguous);
+  // make the tensor contiguous.
+  tensor.empty_tensor_restride(c10::MemoryFormat::Contiguous);
 
-    // Sizes should NOT have changed.
-    EXPECT_EQ(tensor.dim(), 2);
-    EXPECT_THAT(tensor.sizes(), ElementsAreArray({4, 2}));
+  // Sizes should NOT have changed.
+  EXPECT_EQ(tensor.dim(), 2);
+  EXPECT_THAT(tensor.sizes(), ElementsAreArray({4, 2}));
 
-    // Strides SHOULD have changed to be contiguous for a (4, 2) tensor.
-    EXPECT_THAT(tensor.strides(), ElementsAreArray({2, 1}));
+  // Strides SHOULD have changed to be contiguous for a (4, 2) tensor.
+  EXPECT_THAT(tensor.strides(), ElementsAreArray({2, 1}));
 
-    // The contiguity flag should now be true.
-    EXPECT_TRUE(tensor.is_contiguous());
-
+  // The contiguity flag should now be true.
+  EXPECT_TRUE(tensor.is_contiguous());
 }
 
 } // namespace torch::standalone
