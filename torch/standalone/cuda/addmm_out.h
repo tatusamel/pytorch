ee3b1324cb55041c3ec079e0109f449cfa03f6b7
Serhat Gundem <tatusamell@gmail.com>
2025-07-21 11:23:34 -0700

    implement addmm_out

---
+++ torch/standalone/cuda/addmm_out.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <c10/core/Scalar.h>
+#include <torch/standalone/slim_tensor/slim_tensor.h>
+
+namespace torch::standalone {
+
+// CUDA implementation of addmm_out
+void _cuda_addmm_out(
+    const SlimTensor& input,
+    const SlimTensor& mat1,
+    const SlimTensor& mat2,
+    const c10::Scalar& beta,
+    const c10::Scalar& alpha,
+    SlimTensor& out
+);
+
+} // namespace torch::standalone
